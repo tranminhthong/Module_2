43b8406ac9e88ef695cbcebb368c28a7d39a3e28
tranminhthong <minhthong9711@gmail.com>
2021-01-08 16:25:24 +0700

    [Bài tập] Hiển thị lời chào

---
+++ hien-loi-chao/src/Xinchao.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Xinchao {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = a.nextLine();
+        System.out.println("Hello: " + name);
+    }
+}
