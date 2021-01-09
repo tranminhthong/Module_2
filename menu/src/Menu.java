fba35efb08e0d2d0afe1dfd82d3d1b7cb0f002d1
tranminhthong <minhthong9711@gmail.com>
2021-01-09 17:30:45 +0700

    [Thực hành] Thiết kế menu cho ứng dụng

---
+++ menu/src/Menu.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.println("Menu");
+            System.out.println("1. Draw the triangle");
+            System.out.println("2. Draw the square");
+            System.out.println("3. Draw the rectangle");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            int choice;
+            choice = input.nextInt();
+            if (choice == 1) {
+                System.out.println("Draw the triangle");
+                System.out.println("******");
+                System.out.println("*****");
+                System.out.println("****");
+                System.out.println("***");
+                System.out.println("**");
+                System.out.println("*");
+            } else if (choice == 2) {
+                System.out.println("Draw the square");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+            } else if (choice == 3) {
+                System.out.println("Draw the rectangle");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+                System.out.println("* * * * * *");
+            } else if (choice == 0) {
+                System.exit(0);
+            } else {
+                System.out.println("No choice!");
+            }
+        }
+    }
+}
