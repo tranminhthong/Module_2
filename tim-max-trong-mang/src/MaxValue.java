1ae486fc96e4767e9299fbe9c209f40b3aeeee3c
tranminhthong <minhthong9711@gmail.com>
2021-01-10 21:42:31 +0700

    [Thực hành] Tìm giá trị lớn nhất trong mảng

---
+++ tim-max-trong-mang/src/MaxValue.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class MaxValue {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size: ");
+            size = sc.nextInt();
+            if (size > 20) {
+                System.out.println("Size should not exceed 20");
+            }
+        } while (size > 20);
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter element" + (i + 1) + " : ");
+            array[i] = sc.nextInt();
+            i++;
+        }
+        System.out.print("Property list: ");
+        for (int k : array) {
+            System.out.print(k + "\t");
+        }
+        int max = array[0];
+        int index = 1;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] > max) {
+                max = array[j];
+                index = j + 1;
+            }
+        }
+        System.out.println("\n" + "The largest property value in the list is " + max + ", at position " + index);
+    }
+}
