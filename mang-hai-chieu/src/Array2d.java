b813832052efd46a90ad6fbc455ad145063fd41a
tranminhthong <minhthong9711@gmail.com>
2021-01-11 10:58:22 +0700

    [Bài tập] Tìm phần tử lớn nhất trong mảng hai chiều

--- mang-hai-chieu/src/Array2d.java
+++ mang-hai-chieu/src/Array2d.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Array2d {
     public static void main(String[] args) {
-        int[][] arr = {
-                {312, 34, 412},
-                {-213, 4324, 3},
-                {432, 21, 100}
-        };
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập độ dài mảng 2 chiều:");
+        int a = sc.nextInt();
+        System.out.println("Nhập độ dài phần tử của mảng 2 chiều:");
+        int b = sc.nextInt();
+        int[][] arr = new int[a][b];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.print("Nhập phần tử thứ " + j + " của phần tử thứ " + i + " của mảng 2 chiều: ");
+                arr[i][j] = sc.nextInt();
+            }
+        }
         int max = arr[0][0];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
