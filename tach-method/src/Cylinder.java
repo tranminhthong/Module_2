69d730d566228579621fb2f4b14fe69b89a7d171
tranminhthong <minhthong9711@gmail.com>
2021-01-17 22:40:00 +0700

    [Thực hành] Refactoring - tách phương thức

---
+++ tach-method/src/Cylinder.java
@@ -0,0 +1,16 @@
+public class Cylinder {
+    public static double getVolume(int radius, int height) {
+        double baseArea = getBaseArea(radius);
+        double perimeter = getPerimeter(radius);
+        return perimeter * height + 2 * baseArea;
+    }
+
+    private static double getPerimeter(int radius) {
+        return 2 * Math.PI * radius;
+    }
+
+    private static double getBaseArea(int radius) {
+        return Math.PI * radius * radius;
+    }
+}
+
