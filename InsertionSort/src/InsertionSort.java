1f45e122d20c6c3e67826ad1991687cedc5d3cb7
tranminhthong <minhthong9711@gmail.com>
2021-01-27 14:01:20 +0700

    [Bài tập] Minh hoạ thuật toán sắp xếp chèn

--- InsertionSort/src/InsertionSort.java
+++ InsertionSort/src/InsertionSort.java
@@ -1,23 +1,25 @@
 public class InsertionSort {
-    void sort(int[] arr){
+    void sort(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n; i++) {
             int key = arr[i];
-            int j = i-1;
-            while (j>=0&&arr[j]>key){
-                arr[j+1]=arr[j];
-                j = j-1;
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
             }
-            arr[j+1]=key;
+            arr[j + 1] = key;
         }
     }
-    static void printArray(int[] arr){
+
+    static void printArray(int[] arr) {
         for (int e : arr) {
             System.out.print(e + " ");
         }
     }
+
     public static void main(String[] args) {
-        int[] arr = {12,11,13,5,6};
+        int[] arr = {12, 11, 13, 5, 6};
         System.out.println("Mảng ban đầu: ");
         printArray(arr);
         InsertionSort insertionSort = new InsertionSort();
