e82cabe82f4e34f7569027db4b44ed1d26452ceb
tranminhthong <minhthong9711@gmail.com>
2021-02-06 20:24:38 +0700

    [Bài tập] Hiển thị số chẵn/lẻ

---
+++ display_odd_even/src/OddThread.java
@@ -0,0 +1,15 @@
+public class OddThread extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            if (i%2!=0){
+                System.out.println(i);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
